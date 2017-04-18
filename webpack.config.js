821c9631778250c2b74dad9360f15b87b4beced9
isqua <isqua@isqua.ru>
2017-04-18 14:24:50 +0300

    Move src out of extension folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@
 module.exports = [
     {
         entry: {
-            vk: './asocial/src/content/networks/vk.js',
-            fb: './asocial/src/content/networks/facebook.js',
-            tw: './asocial/src/content/networks/twitter.js',
-            ok: './asocial/src/content/networks/odnoklassniki.js'
+            vk: './src/content/networks/vk.js',
+            fb: './src/content/networks/facebook.js',
+            tw: './src/content/networks/twitter.js',
+            ok: './src/content/networks/odnoklassniki.js'
         },
 
         output: {
@@ -14,14 +14,14 @@ module.exports = [
         }
     },
     {
-        entry: './asocial/src/options/options.js',
+        entry: './src/options/options.js',
 
         output: {
             filename: 'asocial/options/options.js'
         }
     },
     {
-        entry: './asocial/src/background.js',
+        entry: './src/background.js',
 
         output: {
             filename: 'asocial/background/background.js'
